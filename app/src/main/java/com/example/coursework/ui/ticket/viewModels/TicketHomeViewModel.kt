f0846386f9716051ee54c54358aeb4e7741b5dee
Vlad_Kamchatny <vlad.kamchatniy@gmail.com>
2024-01-30 17:25:01 +0200

    Delete unused imports.

--- app/src/main/java/com/example/coursework/ui/ticket/viewModels/TicketHomeViewModel.kt
+++ app/src/main/java/com/example/coursework/ui/ticket/viewModels/TicketHomeViewModel.kt
@@ -2,9 +2,7 @@ package com.example.coursework.ui.ticket.viewModels
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.coursework.data.classes.Station
 import com.example.coursework.data.classes.Ticket
-import com.example.coursework.data.repositories.StationsRepository
 import com.example.coursework.data.repositories.TicketsRepository
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
