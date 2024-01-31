f60b360e70582c2342fb65dc2f1d18411a3345fa
Vlad_Kamchatny <vlad.kamchatniy@gmail.com>
2024-01-31 17:36:44 +0200

    Delete unused import.

--- app/src/main/java/com/example/coursework/ui/wagon/screens/WagonDetailsScreen.kt
+++ app/src/main/java/com/example/coursework/ui/wagon/screens/WagonDetailsScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
