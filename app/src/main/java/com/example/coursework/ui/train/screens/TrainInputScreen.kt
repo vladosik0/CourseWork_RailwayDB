5df2f353e7c063669f7d105c69a26722829c86b1
Vlad_Kamchatny <vlad.kamchatniy@gmail.com>
2024-02-04 01:47:51 +0200

    Add time selector for departureDate and arrivalDate attributes when creating or editing train row.

--- app/src/main/java/com/example/coursework/ui/train/screens/TrainInputScreen.kt
+++ app/src/main/java/com/example/coursework/ui/train/screens/TrainInputScreen.kt
@@ -2,10 +2,16 @@ package com.example.coursework.ui.train.screens
 
 import android.annotation.SuppressLint
 import android.widget.Toast
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.DateRange
 import androidx.compose.material3.Button
@@ -14,19 +20,26 @@ import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TimePicker
 import androidx.compose.material3.rememberDatePickerState
+import androidx.compose.material3.rememberTimePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.coursework.ui.AppViewModelProvider
 import com.example.coursework.ui.NavigationDestination
@@ -187,10 +200,26 @@ fun TrainInputForm(
 fun MyDatePickerDialog(
     isDepartureDatePicked: Boolean,
     trainUiState: TrainUiState,
-    onDateSelected: (TrainUiState) -> Unit = {},
+    onDateSelected: (TrainUiState) -> Unit,
     onDismiss: () -> Unit
 ) {
-    val state = rememberDatePickerState()
+    val state =
+        rememberDatePickerState()
+    val openTimePickerDialog = remember { mutableStateOf(false) }
+    if (openTimePickerDialog.value) {
+        MyTimePickerDialog(
+            pickedDate = SimpleDateFormat("dd/MM/yyyy").format(
+                state.selectedDateMillis
+            ),
+            isDepartureDatePicked = isDepartureDatePicked,
+            trainUiState = trainUiState,
+            onDismiss = {
+                openTimePickerDialog.value = false
+                onDismiss()
+            },
+            onTimeSelected = onDateSelected
+        )
+    }
     DatePickerDialog(
         onDismissRequest = {
             onDismiss()
@@ -198,24 +227,7 @@ fun MyDatePickerDialog(
         confirmButton = {
             TextButton(
                 onClick = {
-                    if (isDepartureDatePicked) {
-                        onDateSelected(
-                            trainUiState.copy(
-                                departureDate = SimpleDateFormat("dd/MM/yyyy").format(
-                                    state.selectedDateMillis
-                                )
-                            )
-                        )
-                    } else {
-                        onDateSelected(
-                            trainUiState.copy(
-                                arrivalDate = SimpleDateFormat("dd/MM/yyyy").format(
-                                    state.selectedDateMillis
-                                )
-                            )
-                        )
-                    }
-                    onDismiss()
+                    openTimePickerDialog.value = true
                 }
             ) {
                 Text("OK")
@@ -232,7 +244,103 @@ fun MyDatePickerDialog(
         }
     ) {
         DatePicker(
+            state = state,
+            dateValidator = {
+                it >= System.currentTimeMillis() + 86400000L // equivalent of one day in ms to set adding train minimum on next day
+            }
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyTimePickerDialog(
+    pickedDate: String,
+    isDepartureDatePicked: Boolean,
+    trainUiState: TrainUiState,
+    onTimeSelected: (TrainUiState) -> Unit,
+    onDismiss: () -> Unit
+) {
+    val state = rememberTimePickerState()
+    TimePickerDialog(
+        onConfirm = {
+            if (isDepartureDatePicked) {
+                onTimeSelected(
+                    trainUiState.copy(
+                        departureDate = pickedDate + ' ' + state.hour + ':' + state.minute
+                    )
+                )
+            } else {
+                onTimeSelected(
+                    trainUiState.copy(
+                        arrivalDate = pickedDate + ' ' + state.hour + ':' + state.minute
+                    )
+                )
+            }
+            onDismiss()
+        },
+        onCancel = {
+            onDismiss()
+        }
+    ) {
+        TimePicker(
             state = state
         )
+    }
+}
+
+@Composable
+fun TimePickerDialog(
+    title: String = "Select Time",
+    onCancel: () -> Unit,
+    onConfirm: () -> Unit,
+    toggle: @Composable () -> Unit = {},
+    content: @Composable () -> Unit,
+) {
+    Dialog(
+        onDismissRequest = onCancel,
+        properties = DialogProperties(
+            usePlatformDefaultWidth = false
+        ),
+    ) {
+        Surface(
+            shape = MaterialTheme.shapes.extraLarge,
+            tonalElevation = 6.dp,
+            modifier = Modifier
+                .width(IntrinsicSize.Min)
+                .height(IntrinsicSize.Min)
+                .background(
+                    shape = MaterialTheme.shapes.extraLarge,
+                    color = MaterialTheme.colorScheme.surface
+                ),
+        ) {
+            Column(
+                modifier = Modifier.padding(24.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 20.dp),
+                    text = title,
+                    style = MaterialTheme.typography.labelMedium
+                )
+                content()
+                Row(
+                    modifier = Modifier
+                        .height(40.dp)
+                        .fillMaxWidth()
+                ) {
+                    toggle()
+                    Spacer(modifier = Modifier.weight(1f))
+                    TextButton(
+                        onClick = onCancel
+                    ) { Text("Cancel") }
+                    TextButton(
+                        onClick = onConfirm
+                    ) { Text("OK") }
+                }
+            }
+        }
     }
 }
